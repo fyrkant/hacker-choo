1a7d7d2c6ff21974c177a78e048205e85bd2b64f
Mattias <mw222rs@student.lnu.se>
2016-06-25 15:27:45 +0200

    blinking cursor

--- src/app.js
+++ src/app.js
@@ -5,12 +5,22 @@ sf('./styles.css', {global: true})
 
 const app = choo()
 
+const handleKeyDown = e => {
+  e.preventDefault()
+
+  console.log(e)
+}
+
 const mainView = (params, state, send) =>
-  choo.view`<p>hi</p>`
+  choo.view`
+  <div>
+    <p>hi <span class="cursor">|</span></p>
+  </div>`
 
 app.router(route => [
   route('/', mainView)
 ])
 
 const tree = app.start()
+document.addEventListener('keydown', handleKeyDown)
 document.body.appendChild(tree)
