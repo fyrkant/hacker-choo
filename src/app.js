1868e392667d96c9db0040db80c1b432fb8f78c8
Mattias <mw222rs@student.lnu.se>
2016-06-25 22:59:06 +0200

    main functionality working

--- src/app.js
+++ src/app.js
@@ -1,26 +1,72 @@
 const choo = require('choo')
+const http = require('choo/http')
 const sf = require('sheetify')
 
 sf('./styles.css', {global: true})
 
 const app = choo()
 
-const handleKeyDown = e => {
-  e.preventDefault()
+app.model({
+  state: {
+    index: 0,
+    text: null,
+    text2: null
+  },
+  reducers: {
+    addText: (action, state) => ({ text: action.payload }),
+    increaseIndex: (action, state) => ({ index: state.index + 3 }),
+    decreasIndex: (action, state) => ({ index: state.index - 3 })
+  },
+  subscriptions: [
+    function (send) {
+      onkeydown = (e) => send('handleKeyDown', { payload: e.keyCode })
+    }
+  ],
+  effects: {
+    handleKeyDown: (action, state, send) => {
+      if (action.payload === 8) {
+        send('decreasIndex')
+      } else {
+        send('increaseIndex')
+      }
+    },
+    getData: (action, state, send) => {
+      http.get('/kernel.txt', (err, res, body) => {
+        if (err) console.log(err)
 
-  console.log(e)
+        send('addText', { payload: body })
+      })
+    }
+  }
+})
+
+const codeLiner = (text, index) => {
+  var rtn = new RegExp('\n', 'g') // eslint-disable-line no-control-regex
+  var rts = new RegExp('\\s', 'g') // whitespace regex
+  var rtt = new RegExp('\\t', 'g') // tab regex
+  const formattedText = text && text.substr(0, index)
+                                    .replace(rtn, '<br/>')
+                                    .replace(rtt, '&nbsp;&nbsp;&nbsp;&nbsp;')
+                                    .replace(rts, '&nbsp;')
+
+  const container = document.createElement('span')
+  container.innerHTML = formattedText
+
+  return choo.view`<p>${container}${cursor}</p>`
 }
+const cursor = choo.view`<span class="cursor">|</span>`
 
-const mainView = (params, state, send) =>
-  choo.view`
-  <div>
-    <p>hi <span class="cursor">|</span></p>
-  </div>`
+const mainView = (params, state, send) => {
+  if (state.text === null) send('getData')
+  return choo.view`
+    <div>
+      ${codeLiner(state.text, state.index)}
+    </div>`
+}
 
 app.router(route => [
   route('/', mainView)
 ])
 
 const tree = app.start()
-document.addEventListener('keydown', handleKeyDown)
 document.body.appendChild(tree)
